860b8cafd3c7aaf88cb7d64859f0c526a96615ff
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-09 12:43:37 -0800

    Extended base template to login page and created Customer() class.

--- customers.py
+++ customers.py
@@ -4,6 +4,38 @@
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self, firstname, lastname, email, password):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        """Convenience method to show information about customer in console."""
+
+        return "<Customer: %s %s, %s>" % (self.firstname, self.lastname, self.email)
+
+
+def read_customers_from_file(filepath):
+    """Read customer data and populate dictionary of customers
+
+    Dictionary will be {email: Customer object}
+    """
+
+    customers = {}
+
+    for line in open(filepath):
+        (first_name,
+         last_name,
+         email,
+         password) = line.strip().split("|")
+
+        customers[email] = Customer(first_name,
+                                    last_name,
+                                    email,
+                                    password)
+
+    return customers 
+
 
 
